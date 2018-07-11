b0e7e6bd51e40697a32bf7f8ea3f9ea1b0f78852
Mehmet Toprak <mtoprak53@gmail.com>
2018-07-11 13:56:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 var recipes = { milk: "2 cup" }
 
 function updateObjectWithKeyAndValue(object, key, value) {
+	return Object.assign({}, object, { key: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 	object[key] = value;
 	return object
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-	object.key = value;
-	return object
-}
-
 function deleteFromObjectByKey(object, key) {
-	var newObject = Object.assign({}, object)
-	delete newObject.key;
+	newObject = Object.assign({}, object)
+	delete newObject[key]
 	return newObject
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-	delete object.key;
+	delete object[key]
 	return object
 }
